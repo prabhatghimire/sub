b13974dcdf735933db2aa8ac82f4ab05998d0994
prabhatghimire2 <prabhatghimire2@github.com>
2020-02-12 12:10:00 -0500

    1st

--- sub/settings.py
+++ sub/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -75,7 +76,7 @@ ROOT_URLCONF = 'sub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/sustainable/sub/static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+#STATIC_ROOT = '/home/sustainable/sub/static'
 
 
 
